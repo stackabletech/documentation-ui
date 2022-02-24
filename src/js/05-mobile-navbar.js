f9ec22a951f0cefb75809b310d11d8cfa169d113
Sven Petersen <s.petersen@webever.de>
2022-02-24 19:13:18 +0100

    mobile nav implementation

--- src/js/05-mobile-navbar.js
+++ src/js/05-mobile-navbar.js
@@ -6,15 +6,34 @@
   navbarBurger.addEventListener('click', toggleNavbarMenu.bind(navbarBurger))
 
   function toggleNavbarMenu (e) {
-    e.stopPropagation() // trap event
-    document.documentElement.classList.toggle('is-clipped--navbar')
+    e.stopPropagation()
+    //document.documentElement.classList.toggle('is-clipped--navbar')
     this.classList.toggle('is-active')
     var menu = document.getElementById(this.dataset.target)
+
+    if (!menu.classList.contains('is-active')) {
+      menu.classList.add('is-active')
+      menu.style.height = 'auto'
+      var height = menu.clientHeight + 'px'
+      menu.style.height = '0px'
+
+      setTimeout(() => {
+        menu.style.height = height
+      }, 0)
+    } else {
+      menu.style.height = '0px'
+      menu.addEventListener('transitionend', () => {
+        menu.classList.remove('is-active')
+        menu.style.height = 'auto'
+      }, { once: true })
+    }
+    /*
     if (menu.classList.toggle('is-active')) {
       menu.style.maxHeight = ''
       var expectedMaxHeight = window.innerHeight - Math.round(menu.getBoundingClientRect().top)
       var actualMaxHeight = parseInt(window.getComputedStyle(menu).maxHeight, 10)
       if (actualMaxHeight !== expectedMaxHeight) menu.style.maxHeight = expectedMaxHeight + 'px'
     }
+    */
   }
 })()
